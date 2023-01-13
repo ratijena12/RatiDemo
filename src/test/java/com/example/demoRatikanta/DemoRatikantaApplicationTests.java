21819a6cd0a1cd6b634e05752c717df165ccc861
ratijena12 <ratikanta186@gmail.com>
2023-01-13 03:28:57 +0000

    some changes

---
+++ src/test/java/com/example/demoRatikanta/DemoRatikantaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoRatikanta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoRatikantaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
